db509022ddefa132074e460a60b45e165cba4c81
Michael Jones <mtjones1979@yahoo.com>
2021-03-21 16:32:38 -0500

    set up connection.js to call mysql

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,21 @@
 // connection to mysql - host, local host, password, database
-
 const mysql = require('mysql');
 // const util = require('util');
-const connection = mysql.createConnection ({
+
 // host, user,database, password
+const connection = mysql.createConnection ({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Coding2021!',
+  database: 'employee_tracker',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  connection.end();
 })
-connection.connect();
 // connection.query = util.promisify(connection.query);
 
 module.exports = connection;
